65cce49060aeb5f169c6ba39292aefad059fabb6
Dmitry Baranov <dbr@sitecore.net>
2017-03-21 12:53:24 +0200

    Initial release.

---
+++ src/Sitecore.Support.106835/InboundContactIdentifierFilter.cs
@@ -0,0 +1,40 @@
+﻿using Sitecore.Analytics.Aggregation.Data.Model;
+using Sitecore.Analytics.Aggregation.Pipelines.ContactProcessing;
+using Sitecore.Analytics.Data;
+using Sitecore.Analytics.Model.Entities;
+using Sitecore.ContentSearch.Analytics.Pipelines.AggregationFilters;
+
+
+namespace Sitecore.Support.ContentSearch.Analytics.Pipelines.AggregationFilters
+{
+    public class InboundContactIdentifierFilter : AggregationInboundFilterProcessor
+    {
+
+        protected override void DoProcess(AgregationFilterPipelineArgs<IVisitAggregationContext> args)
+        {
+            this.DoProcess(args, args.Context.Contact);
+        }
+
+        protected override void DoProcess(AgregationFilterPipelineArgs<ContactProcessingArgs> args)
+        {
+            if (args.Context.Reason != ProcessingReason.Obsoleted)
+            {
+                this.DoProcess(args, args.Context.GetContact());
+            }
+        }
+
+        protected void DoProcess(AgregationFilterPipelineArgs args, IContact contact)
+        {
+            string identifier;
+            if (contact != null)
+            {
+                identifier = contact.Identifiers.Identifier;
+                args.IsExcluded = string.IsNullOrEmpty(identifier);
+                if (args.IsExcluded)
+                {
+                    args.FilteredMessage = "Filtered by Identifier";
+                }
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.106835/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.106835")]
+[assembly: AssemblyProduct("Sitecore.Support.106835")]
+[assembly: ComVisible(false)]
